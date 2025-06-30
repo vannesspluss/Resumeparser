047b0024516875361f325d40b0bfcbe5214adc03
Jiraporn1008 <jiraporn.wang@mail.kmutt.ac.th>
2025-06-30 16:31:03 +0700

    commit

--- parser.py
+++ parser.py
@@ -1,17 +1,11 @@
 import os
 from openai import OpenAI
 
-api_key = None
-CONFIG_PATH = r"config.yaml"
-
-# api_key = os.getenv("OPENAI_API_KEY")
-# Setup API Key
-api_key = os.environ["OPENAI_API_KEY"]
+api_key = os.getenv("OPENAI_API_KEY")
 
 def ats_extractor(resume_data):
-
     prompt = '''
-    You are an AI bot designed to act as a professional for parsing resumes. You are given with resume and your job is to extract the following information from the resume:
+    You are an AI bot designed to act as a professional for parsing resumes. Extract the following info from the resume:
     1. full name
     2. email id
     3. github portfolio
@@ -19,29 +13,21 @@ def ats_extractor(resume_data):
     5. employment details
     6. technical skills
     7. soft skills
-    Give the extracted information in json format only
+    Provide the output ONLY in JSON format.
     '''
 
-    openai_client = OpenAI(
-        api_key = api_key
-    )    
+    openai_client = OpenAI(api_key=api_key)
 
-    messages=[
-        {"role": "system", 
-        "content": prompt}
-        ]
-    
-    user_content = resume_data
-    
-    messages.append({"role": "user", "content": user_content})
+    messages = [
+        {"role": "system", "content": prompt},
+        {"role": "user", "content": resume_data}
+    ]
 
     response = openai_client.chat.completions.create(
-                model="gpt-3.5-turbo",
-                messages=messages,
-                temperature=0.0,
-                max_tokens=1500)
-        
-    data = response.choices[0].message.content
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0,
+        max_tokens=1500
+    )
 
-    #print(data)
-    return data
+    return response.choices[0].message.content
